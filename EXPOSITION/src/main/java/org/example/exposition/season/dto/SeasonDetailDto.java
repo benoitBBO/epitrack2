850cd3d2a89ee545371840312109331d9514921f
DESKTOP-8D8MR00\sandrine binutti <sandrine.binutti@epita.fr>
2023-07-21 11:08:11 +0200

    Serie/Saison DTO corrections

--- EXPOSITION/src/main/java/org/example/exposition/season/dto/SeasonDetailDto.java
+++ EXPOSITION/src/main/java/org/example/exposition/season/dto/SeasonDetailDto.java
@@ -8,23 +8,74 @@ import java.time.LocalDate;
 import java.util.List;
 
 public class SeasonDetailDto {
-    private SeasonDetailWithoutEpisodeDto season;
+    private Long id;
+    private String title;
+    private Integer seasonNumber;
+    private String overview;
+    private String imageUrl;
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate airDate;
     private List<EpisodeDetailDto> episodes;
 
     public SeasonDetailDto() {
     }
 
-    public SeasonDetailDto(SeasonDetailWithoutEpisodeDto season, List<EpisodeDetailDto> episodes) {
-        this.season = season;
+    public SeasonDetailDto(Long id, String title, Integer seasonNumber, String overview, String imageUrl, LocalDate airDate, List<EpisodeDetailDto> episodes) {
+        this.id = id;
+        this.title = title;
+        this.seasonNumber = seasonNumber;
+        this.overview = overview;
+        this.imageUrl = imageUrl;
+        this.airDate = airDate;
         this.episodes = episodes;
     }
 
-    public SeasonDetailWithoutEpisodeDto getSeason() {
-        return season;
+    public Long getId() {
+        return id;
     }
 
-    public void setSeason(SeasonDetailWithoutEpisodeDto season) {
-        this.season = season;
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getSeasonNumber() {
+        return seasonNumber;
+    }
+
+    public void setSeasonNumber(Integer seasonNumber) {
+        this.seasonNumber = seasonNumber;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public LocalDate getAirDate() {
+        return airDate;
+    }
+
+    public void setAirDate(LocalDate airDate) {
+        this.airDate = airDate;
     }
 
     public List<EpisodeDetailDto> getEpisodes() {
